10412d5af04511432d83b4d23bc41acc8352a5bb
Mikael Karon <mikael@karon.se>
2018-09-27 23:40:07 +0200

    allow either tree or ref for path

--- controller.js
+++ controller.js
@@ -2,9 +2,15 @@ const { NEXT } = require('./const');
 
 module.exports = repo => ({
   loadPath: async (req, res) => {
-    const { tree, path } = req.params;
-    const result = await repo.loadTextByPath(tree, path);
+    const { path } = req.params;
+    let { tree } = req.params;
 
+    let ref = await repo.getRef(`refs/heads/${tree}`);
+    if (ref) {
+      ({ body: { tree } } = await repo.loadObject(ref));
+    }
+
+    const result = await repo.loadTextByPath(tree, path);
     if (result) {
       res.send(result);
     }
@@ -15,8 +21,8 @@ module.exports = repo => ({
 
   loadBlob: async (req, res) => {
     const { blob } = req.params;
-    const result = await repo.loadText(blob);
 
+    const result = await repo.loadText(blob);
     if (result) {
       res.send(result);
     }
