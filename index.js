27c7b69f46e2980856136a2ecc09e0aec03ba862
Mikael Karon <mikael@karon.se>
2018-09-29 04:00:37 +0200

    remove deps to @es-git/path-to-object-mixin

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const { default: FsRepo } = require('@es-git/node-fs-repo');
 const { default: zlibMixin } = require('@es-git/zlib-mixin');
 const { default: objectMixin } = require('@es-git/object-mixin');
 const { default: loadAsMixin } = require('@es-git/load-as-mixin');
-const { default: pathToObjectMixin } = require('@es-git/path-to-object-mixin');
 const { PORT, PATH } = require('./const');
 
 const Repo = mix(FsRepo)
   .with(zlibMixin)
   .with(objectMixin)
-  .with(loadAsMixin)
-  .with(pathToObjectMixin);
+  .with(loadAsMixin);
 
 express()
   .use(require('@mu-cms/express-es-git')(new Repo(PATH)))
