2716a921f57a2c7aa8ffb4a25bc812fda2adbf82
Mikael Karon <mikael@karon.se>
2018-09-30 14:03:48 +0200

    add support for fs/mem repor and fetch

--- const.js
+++ const.js
@@ -2,3 +2,5 @@ const path = require('path');
 
 exports.PORT = 8080;
 exports.PATH = path.join(__dirname, '.git');
+exports.URL = 'https://github.com/mu-cms/mu-express.git';
+exports.REFSPEC = 'refs/heads/*:refs/heads/*';
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const git = require('@mu-cms/express-es-git');
-const { FsRepo } = require('./repo');
-const { PORT, PATH } = require('./const');
+const { FsRepo, MemRepo } = require('./repo');
+const { PORT, PATH, URL, REFSPEC } = require('./const');
 
-const repo = new FsRepo(PATH);
+let repo;
+if (PATH) {
+  console.log(`using fs repo at ${PATH}`);
+  repo = new FsRepo(PATH);
+}
+else {
+  console.log('using mem repo');
+  repo = new MemRepo();
+}
 
 express()
   .use(git(repo))
-  .listen(PORT, () => console.log(`app started on ${PORT}`));
+  .listen(PORT, async () => {
+    if (URL) {
+      console.log(`fetching ${REFSPEC || 'default refspec'} from ${URL}`)
+      await repo.fetch(URL, REFSPEC);
+    }
+    console.log(`app started on ${PORT}`);
+  });
