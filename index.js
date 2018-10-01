3e0a3d50bdd6a7f4dcbd7fda961edcfcc2817252
Mikael Karon <mikael@karon.se>
2018-10-01 22:39:02 +0200

    use dotenv and lates es-git middleware

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const git = require('@mu-cms/express-es-git');
 const { FsRepo, MemRepo } = require('./repo');
-const { PORT, GIT_PATH, GIT_URL, GIT_REFSPEC } = process.env;
+const { PORT, GIT } = process.env;
 
 let repo;
-if (GIT_PATH) {
-  console.log(`using fs repo at ${GIT_PATH}`);
-  repo = new FsRepo(GIT_PATH);
+if (GIT) {
+  console.log(`using fs repo at ${GIT}`);
+  repo = new FsRepo(GIT);
 }
 else {
   console.log('using mem repo');
@@ -16,13 +16,6 @@ else {
 }
 
 express()
+  .use(express.urlencoded({ extended: true }))
   .use(git(repo))
-  .listen(PORT, async () => {
-    if (GIT_URL) {
-      console.log(`fetching ${GIT_REFSPEC || 'default'} from ${GIT_URL}`);
-      await repo.fetch(GIT_URL, GIT_REFSPEC, {
-        progress: message => console.log(message)
-      });
-    }
-    console.log(`app started on ${PORT}`);
-  });
+  .listen(PORT, async () => console.log(`app started on ${PORT}`));
